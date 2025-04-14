9dc9f413035139ae8f288fe0efa5b2f150701c1c
yangyixin130 <1578111082@qq.com>
2025-04-14 22:16:15 +0800

    Add:init my vue-project

---
+++ vueTailiwind/tailwind.config.js
@@ -0,0 +1,20 @@
+/*
+ * @Author: yangyixin130 1578111082@qq.com
+ * @Date: 2025-04-14 22:02:57
+ * @LastEditors: yangyixin130 1578111082@qq.com
+ * @LastEditTime: 2025-04-14 22:05:11
+ * @FilePath: /study-materials/vue3TalidwindTs/vue3TalidwindTs/vueTailiwind/tailwind.config.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
